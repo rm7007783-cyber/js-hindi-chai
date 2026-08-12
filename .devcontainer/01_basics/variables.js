99ec70d1ce7a74719cb6de141f7bb6268b946491
rm7007783-cyber <rm7007783@gmail.com>
2026-08-12 19:20:43 +0000

    datatypes

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountID=14432
+let accountEmail="rajm@gmail.com"
+var accountpassword="23434"
+accountcity="jaipur"
+// accountID=2 not allowed
+let accountState;
+
+accountEmail="hc@gmaiil.com"
+accountpassword="433553"
+accountcity="kota"
+console.log(accountID);
+console.table([accountID, accountEmail, accountcity, accountpassword, accountState])
